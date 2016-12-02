6f389d42834c831542adde3189ae886f5aa7e22f
sen.lyu <sen.lyu@uconn.edu>
2016-12-02 14:34:25 -0500

    changes in two line add

--- python-leet/addtwonumbers.py
+++ python-leet/addtwonumbers.py
@@ -1,20 +1,20 @@
-def addTwoNumbers(l1, l2):
-    """
-    :type l1: ListNode
-    :type l2: ListNode
-    :rtype: ListNode
-    """
-    carry=0
-    n=root=listnode(0)
-    while l1 or l2 or carry:
-        if l1:
-            carry+=l1.val
-            l1=l1.next
-        if l2:
-            carry+=l2.val
-            l2=l2.next
-        carry, val=divmod(carry,10)
-        n.next=listnode(val)
-        n=n.next
-    return root.next
-
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        carry=0
+        n=root=ListNode(0)
+        while l1 or l2 or carry:
+            if l1:
+                carry+=l1.val
+                l1=l1.next
+            if l2:
+                carry+=l2.val
+                l2=l2.next
+            carry, val=divmod(carry,10)
+            n.next=ListNode(val)
+            n=n.next
+        return root.next
