672c24ff16f08eee58453e9f499eaacc78b51f6d
senlyu <sen.lyu@uconn.edu>
2016-12-03 20:43:59 +0000

    question5 finish

--- python-leet/LongestPalindromicSubstring.py
+++ python-leet/LongestPalindromicSubstring.py
@@ -1,12 +1,15 @@
+s='babad'
 maxn=0
 list3=[]
 for i in range(len(s)):
     for j in range(i,len(s)):
         list1=[]
-        for k in range(i,j):
+        for k in range(i,j+1):
             list1.append(s[k])
-            list2=list1.reverse
-            print(list1,i,j,k)
+            list2=list1[:]
+            list2.reverse()
+            print('print the list the ijk',list1,list2,i,j,k,k-i+1,maxn)
             if list1==list2 and k-i+1>maxn:
                 list3=list1
-                print(list3)
+                maxn=k-i+1
+                print('print the list3',list3)
