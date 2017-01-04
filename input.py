27768f9795574f189f78e092e69a9c55f09d2555
sen.lyu <sen.lyu@uconn.edu>
2017-01-04 03:56:56 -0500

    input

--- input.py
+++ input.py
@@ -1,3 +1,15 @@
+from __future__ import print_function
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+import tarfile
+from IPython.display import display, Image
+from scipy import ndimage
+from sklearn.linear_model import LogisticRegression
+from six.moves.urllib.request import urlretrieve
+from six.moves import cPickle as pickle
+
 url = 'http://commondatastorage.googleapis.com/books1000/'
 last_percent_reported = None
 
